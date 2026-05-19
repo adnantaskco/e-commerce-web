f381ab4f1bcd9ee4dec9202b1181fc502178d68f
sradnan <adnan.taskco@gmail.com>
2026-05-20 01:00:03 +0600

    all updated code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,26 +7,22 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-
-  theme: {
     extend: {
       colors: {
         primary: {
-          50:  '#E38B75',
-          100: '#dbeafe',
-          500: '#E38B75',  // main shade
-          600: '#2563eb',
-          700: '#1d4ed8',
+          50: "#E38B75",
+          100: "#dbeafe",
+          500: "#E38B75", // main shade
+          600: "#2563eb",
+          700: "#1d4ed8",
         },
         secound: {
-          50: '#1E1E1E',
-          100: '#1E1E1E',
-          500: '#1E1E1E',
-        }
+          50: "#1E1E1E",
+          100: "#1E1E1E",
+          500: "#1E1E1E",
+        },
       },
     },
-  },
-}
   },
   plugins: [],
 };
